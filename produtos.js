f8208665642ccf1cce93060d8b95f1d350619bc3
José Mariano da Silva <jose.mariano@climatempo.com.br>
2025-07-15 19:38:54 -0300

    feat: Add page to show products

---
+++ produtos.js
@@ -0,0 +1,50 @@
+const PRODUTOS = {
+  estacao: [
+    {
+      name: "Monitor Dell 24” Full HD",
+      image: "https://m.media-amazon.com/images/I/71KR2i6-WaL._AC_SL1500_.jpg",
+      link: "https://www.amazon.com.br/dp/B08F6542KS"
+    },
+    {
+      name: "Teclado Mecânico Logitech MX",
+      image: "https://m.media-amazon.com/images/I/61j5J+3PfEL._AC_SL1500_.jpg",
+      link: "https://www.amazon.com.br/dp/B08YJFSQQ5"
+    }
+  ],
+  livros: [
+    {
+      name: "Código Limpo — Robert C. Martin",
+      image: "https://m.media-amazon.com/images/I/51b7XbfMIIL._SX379_BO1,204,203,200_.jpg",
+      link: "https://www.amazon.com.br/dp/8576082675"
+    },
+    {
+      name: "O Programador Pragmático",
+      image: "https://m.media-amazon.com/images/I/41E7bAo5bnL._SX342_SY445_.jpg",
+      link: "https://www.amazon.com.br/dp/8576082675"
+    }
+  ],
+  ergonomia: [
+    {
+      name: "Suporte Ajustável para Notebook",
+      image: "https://m.media-amazon.com/images/I/61kQdlpM7tL._AC_SL1500_.jpg",
+      link: "https://www.amazon.com.br/dp/B08CYCKVR9"
+    },
+    {
+      name: "Cadeira Ergonômica Presidente",
+      image: "https://m.media-amazon.com/images/I/61qIuK44ZJL._AC_SL1000_.jpg",
+      link: "https://www.amazon.com.br/dp/B09G94SZMP"
+    }
+  ],
+  gadgets: [
+    {
+      name: "Hub USB-C com HDMI e USB 3.0",
+      image: "https://m.media-amazon.com/images/I/51kM+9EK5lL._AC_SL1000_.jpg",
+      link: "https://www.amazon.com.br/dp/B08L3QZJYZ"
+    },
+    {
+      name: "Mouse Logitech MX Master 3",
+      image: "https://m.media-amazon.com/images/I/61cwywLZR-L._AC_SL1500_.jpg",
+      link: "https://www.amazon.com.br/dp/B07YWF9Q7V"
+    }
+  ]
+};
