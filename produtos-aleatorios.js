1dd79da3f033ed5e9d825426d95c319e46610af9
José Mariano da Silva <jose.mariano@climatempo.com.br>
2025-07-16 20:14:17 -0300

    feat: Add page with random products

---
+++ produtos-aleatorios.js
@@ -0,0 +1,9 @@
+const PRODUTOS = {
+  'Aleatórios': [
+    {
+      name: "Escova Dental Colgate ZigZag Carvão 4 unid",
+      image: "https://m.media-amazon.com/images/I/61gDkDzLzJL._AC_SX425_.jpg",
+      link: "https://amzn.to/4kIhr4m"
+    }
+  ]
+};
