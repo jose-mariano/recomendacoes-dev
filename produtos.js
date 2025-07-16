3195fedc53fd3b00bb18ac832ecdd9655e468d9d
José Mariano da Silva <jose.mariano@climatempo.com.br>
2025-07-16 20:46:22 -0300

    refactor: Add products on Estação category

--- produtos.js
+++ produtos.js
@@ -1,14 +1,44 @@
 const PRODUTOS = {
-  estacao: [
+  'estação': [
     {
-      name: "Monitor Dell 24” Full HD",
-      image: "https://m.media-amazon.com/images/I/71KR2i6-WaL._AC_SL1500_.jpg",
-      link: "https://www.amazon.com.br/dp/B08F6542KS"
+      name: "Notebook Lenovo IdeaPad 1i i5-1235U 8GB 256GB 15.6\" W11",
+      image: "https://m.media-amazon.com/images/I/51BDQoOSZZL._AC_SL1000_.jpg",
+      link: "https://amzn.to/3TK60OH"
     },
     {
-      name: "Teclado Mecânico Logitech MX",
-      image: "https://m.media-amazon.com/images/I/61j5J+3PfEL._AC_SL1500_.jpg",
-      link: "https://www.amazon.com.br/dp/B08YJFSQQ5"
+      name: "Notebook Lenovo LOQ-e 15IAX9E Intel Core i5-12450HX 16GB 512GB SSD RTX 3050 Windows 11 15.6\" - 83ME0007BR Luna Grey",
+      image: "https://m.media-amazon.com/images/I/81vDa3d63CL._AC_SL1500_.jpg",
+      link: "https://amzn.to/458HI7u"
+    },
+    {
+      name: "Teclado Mecânico Redragon Dark Avenger Preto Iluminação Rainbow Switch Marrom K568R",
+      image: "https://m.media-amazon.com/images/I/61KrUwmOvTL._AC_SL1000_.jpg",
+      link: "https://amzn.to/4lrZAQx"
+    },
+    {
+      name: "Teclado Mecânico Gamer T-Dagger Bora Iluminação Rainbow T-TGK313-BR",
+      image: "https://m.media-amazon.com/images/I/61+OMyyoyhL._AC_SL1500_.jpg",
+      link: "https://amzn.to/40VaENL"
+    },
+    {
+      name: "Mouse Gamer Redragon Cobra, Chroma RGB, 10000DPI, 7 Botões, Preto",
+      image: "https://m.media-amazon.com/images/I/61saZHQI52L._AC_SL1500_.jpg",
+      link: "https://amzn.to/4eZFyuq"
+    },
+    {
+      name: "Mouse Gamer Redragon Predator RGB M612-RGB, Preto",
+      image: "https://m.media-amazon.com/images/I/51AVGfhKbOL._AC_SL1500_.jpg",
+      link: "https://amzn.to/44Amtve"
+    },
+    {
+      name: "Monitor LG 19.5 LED HDMI, D-SUB ,VESA Preto 20MK400H",
+      image: "https://m.media-amazon.com/images/I/61FgJP6Fv0L._AC_SL1000_.jpg",
+      link: "https://amzn.to/4nU2Gyq"
+    },
+    {
+      name: "Monitor LG LED 24 Full HD HDMI 5 ms 24MK430H",
+      image: "https://m.media-amazon.com/images/I/51ZCTlMfEVL._AC_SL1024_.jpg",
+      link: "https://amzn.to/44y6rBY"
     }
   ],
   livros: [
