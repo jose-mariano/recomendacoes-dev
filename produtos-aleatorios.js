d6c366f2564cb902989a1f1855d4870b517869cf
José Mariano da Silva <jose.mariano@climatempo.com.br>
2025-07-18 11:50:51 -0300

    refactor: Update links

--- produtos-aleatorios.js
+++ produtos-aleatorios.js
@@ -3,7 +3,7 @@ const PRODUTOS = {
     {
       name: "Escova Dental Colgate ZigZag Carvão 4 unid",
       image: "https://m.media-amazon.com/images/I/61gDkDzLzJL._AC_SX425_.jpg",
-      link: "https://amzn.to/4kIhr4m"
+      link: "https://www.amazon.com.br/dp/B07YM71S4D?psc=1&social_share=cm_sw_r_cso_wa_apan_ct_WFEGG99TQJGAFF2PSYDP&previewDohEventScheduleTesting=C&linkCode=ll1&tag=devexplica-20&ascsubtag=recomendacoes-dev&linkId=8b3434926e720d0be61c1ebead7e707b&language=pt_BR&ref_=as_li_ss_tl"
     }
   ]
 };
